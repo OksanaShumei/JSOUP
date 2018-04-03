ab9d03695749ebc3edb0c40317a7d5eb3eb465c1
Oksana <kgajxel@gmail.com>
2018-04-03 15:13:25 +0300

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,18 +4,15 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException{
 
-        ArrayList<Integer> list = new ArrayList<Integer>();
-
         //отримуємо html-код сторінки
-        Document document = Jsoup.connect("https://free-proxy-list.net/").get();
+        Document doc = Jsoup.connect("https://free-proxy-list.net/").get();
 
         //знаходимо таблицю в документі
-        Element table = document.select("table").first();
+        Element table = doc.select("table").first();
 
         //розбиваємо на рядки по тегу <tr>
         Elements rows = table.select("tr");
@@ -23,9 +20,11 @@ public class Main {
         for (int i = 1; i < rows.size(); i++){
             Element row = rows.get(i); // по номеру індексу отримуємо рядок
             Elements cols = row.select("td"); // розбиваємо отриманий рядок по тегу <td> на стовпці
-            System.out.println(cols.get(0).text()); // IP Address
-            System.out.println(cols.get(1).text()); // Port
-            System.out.println();
+            if (cols.size() > 2) {
+                System.out.println(cols.get(0).text()); // IP Address
+                System.out.println(cols.get(1).text()); // Port
+                System.out.println();
+            }
         }
     }
 }
